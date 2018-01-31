a5bab5ce4a8422165942e3c1abe5070ac693de80
Tiffany Lin <tlin625@gmail.com>
2018-01-31 11:32:51 -0800

    (added) PostgreSQL database

--- app/App.jsx
+++ app/App.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, {Component} from "react";
+// import pool from '../back-end/db';
 
-const App = () => <h1>hello world</h1>
+
+// const App = () => {
+// 	return <h1>hello world</h1>
+// }
+
+class App extends Component {
+	componentDidMount() {
+
+	}
+	render() {
+		return <h1>hello world!!!!!!</h1>
+	}
+}
+
 
 export default App;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const pool = require('./db');
+
+pool.connect();
+// const http = require('http').Server(app);
+
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello World</h1>');
+// });
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
